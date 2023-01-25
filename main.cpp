1e6338648de5d9a8ab8adefe3ea91189a065ecc7
Prakhar Jagwani <prakharjagwani7@gmail.com>
2023-01-25 16:23:52 +0000

    supersampling

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ class SoftwareRasterizer{
     SDL_Surface* framebuffer = NULL;
     SDL_Window* window = NULL;
     SDL_Surface* windowSurface = NULL;
+    int supersampling = 1;
     
     public:
     bool quit = false;
@@ -86,19 +87,45 @@ class SoftwareRasterizer{
         Uint32 *pixels = (Uint32*)framebuffer->pixels;
         SDL_PixelFormat *format = framebuffer->format;
         color *= 255;
+
+        float step = 1.0/(supersampling+1);
+
+        // v1, v2, v3 should be in clockwise order
+        if(glm::cross(v2-v1,v3-v1)[2]<0){
+            glm::vec3 temp{v2};
+            v2 = v3;
+            v3 = temp;
+        }
+
         for (int i = 0; i < frameWidth; i++) {
             for (int j = 0; j < frameHeight; j++) {
-                glm::vec3 pos{i+0.5, j+0.5, 0};
-                if(glm::cross(v2-v1,pos-v1)[2]<0){
-                    continue;
+                // use previous color as default
+                Uint8 r,g,b, a;
+                SDL_GetRGBA(
+                    pixels[i + frameWidth*(frameHeight - 1 - j)],
+                    format, &r, &g, &b, &a);
+                glm::ivec4 default_color{r,g,b,a};
+
+                glm::ivec4 pixel_color{0,0,0,0};
+                for(int s_i = 1; s_i <= supersampling; s_i++){
+                    for(int s_j = 1; s_j <= supersampling; s_j++){
+                        glm::vec3 pos{i+s_i*step, j+s_j*step, 0};
+                        if(
+                            glm::cross(v2-v1,pos-v1)[2]>0 &&
+                            glm::cross(v3-v2,pos-v2)[2]>0 && 
+                            glm::cross(v1-v3,pos-v3)[2]>0
+                        ){
+                            pixel_color += color;
+                        }
+                        else{
+                            pixel_color += default_color;
+                        }          
+                    }                    
                 }
-                if(glm::cross(v3-v2,pos-v2)[2]<0){
-                    continue;
-                }
-                if(glm::cross(v1-v3,pos-v3)[2]<0){
-                    continue;
-                }
-                pixels[i + frameWidth*(frameHeight - 1 - j)] = SDL_MapRGBA(format, color[0], color[1], color[2], color[3]);
+                // average over number of samples
+                pixel_color /= supersampling*supersampling;
+                
+                pixels[i + frameWidth*(frameHeight - 1 - j)] = SDL_MapRGBA(format, pixel_color[0], pixel_color[1], pixel_color[2], pixel_color[3]);
             }
         }
     }
@@ -125,6 +152,11 @@ class SoftwareRasterizer{
         SDL_UpdateWindowSurface(window);
     }
 
+    void set_supersampling(int x){
+        // number of samples per row and per column
+        supersampling = max(round(sqrt(x)),1.0);
+    }
+
     ~SoftwareRasterizer() {
         // Free resources and close SDL
         SDL_FreeSurface(framebuffer);
@@ -134,9 +166,11 @@ class SoftwareRasterizer{
 };
 
 int main(int argc, char* args[]) {
-    SoftwareRasterizer sR{50,50,4};
+    SoftwareRasterizer sR{100,100,4};
     
-    sR.clear(glm::vec4(1,1,0,1));
+    sR.set_supersampling(16);
+
+    sR.clear(glm::vec4(1,1,1,1));
     vector<glm::vec4> vertices = {
             glm::vec4(-0.8, 0.0, 0.0, 1.0),
             glm::vec4(-0.4, -0.8, 0.0, 1.0),
