59a1d0d4c49270633bec5979971afd6865473e2a
Prakhar Jagwani <prakharjagwani7@gmail.com>
2023-01-25 20:11:01 +0530

    draw triangle

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+#include <glm/common.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/geometric.hpp>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
@@ -9,6 +14,8 @@ class SoftwareRasterizer{
     const int displayScale;
     // Output file
     const char* outputFile = "out.png";
+
+    SDL_Renderer* renderer = NULL;
     SDL_Surface* framebuffer = NULL;
     SDL_Window* window = NULL;
     SDL_Surface* windowSurface = NULL;
@@ -47,24 +54,56 @@ class SoftwareRasterizer{
         IMG_SavePNG(framebuffer, outputFile);
     }
 
-    void render(){
+    void clear(glm::vec4 color){
+        // Select the color for drawing. It is set to red here.
+        color *= 255;
+        SDL_Rect framerect {0,0,frameWidth, frameHeight};
+        SDL_PixelFormat *format = framebuffer->format;
+        Uint32 bgColor = SDL_MapRGBA(format, color[0], color[1], color[2], color[3]);
+        SDL_FillRect(framebuffer, &framerect, bgColor);
+    }
+
+    void draw_triangle(glm::vec4 v4_1, glm::vec4 v4_2, glm::vec4 v4_3, glm::vec4 color){
+        glm::vec3 v1{v4_1}, v2{v4_2}, v3{v4_3};
+        glm::mat3x3 scale{
+            frameWidth,0,0,
+            0,frameHeight,0,
+            0,0,1
+        };
+        v1 = scale * v1;
+        v2 = scale * v2;
+        v3 = scale * v3;
+        
         Uint32 *pixels = (Uint32*)framebuffer->pixels;
         SDL_PixelFormat *format = framebuffer->format;
+        color *= 255;
         for (int i = 0; i < frameWidth; i++) {
             for (int j = 0; j < frameHeight; j++) {
-                float x = i + 0.5;
-                float y = j + 0.5;
-                float r = 4;
-                Uint32 color;
-                if ((x-5)*(x-5) + (y-5)*(y-5) <= r*r) { // circle
-                    color = SDL_MapRGBA(format, 0, 153, 0, 255); // green
-                } else {
-                    color = SDL_MapRGBA(format, 255, 255, 255, 255); // white
+                glm::vec3 pos{i+0.5, j+0.5, 0};
+                if(glm::cross(v2-v1,pos-v1)[2]<0){
+                    continue;
                 }
-                pixels[i + frameWidth*j] = color;
+                if(glm::cross(v3-v2,pos-v2)[2]<0){
+                    continue;
+                }
+                if(glm::cross(v1-v3,pos-v3)[2]<0){
+                    continue;
+                }
+                pixels[i + frameWidth*j] = SDL_MapRGBA(format, color[0], color[1], color[2], color[3]);
             }
         }
+    }
 
+    void draw(){
+        draw_triangle(
+            glm::vec4{0.3,0.3,0,0},
+            glm::vec4{0.7,0.1,0,0},
+            glm::vec4{0.4,0.7,0,0},
+            glm::vec4{1,0,0,1}
+        );
+    }
+
+    void render(){
         // Update screen to apply the changes
         SDL_BlitScaled(framebuffer, NULL, windowSurface, NULL);
         SDL_UpdateWindowSurface(window);
@@ -79,13 +118,11 @@ class SoftwareRasterizer{
 };
 
 int main(int argc, char* args[]) {
-    SoftwareRasterizer sR{10,10,40};
-    // Display and interaction
-    while (!sR.quit) {
-        // Event handling
-        sR.handleEvents();
-        sR.render();
-    }
+    SoftwareRasterizer sR{100,100,4};
+    
+    sR.clear(glm::vec4(1,1,0,1));
+    sR.draw();
+    sR.render();
 	// Save image
     sR.saveFramebuffer();
     return 0;
