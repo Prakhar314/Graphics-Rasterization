f9ed43de78c9f2d37337f2951d9a4cc65faba990
Rupesh Kumar <rupesh@Rupeshs-MacBook-Air.local>
2023-02-01 16:12:18 +0530

    added the example for detph testing

--- examples/e3.cpp
+++ examples/e3.cpp
@@ -13,8 +13,8 @@ int main() {
         r.fsConstant()
     );
     vec4 vertices[] = {
-		vec4(0.0, 0.0, 0.0, 1.0),
-        vec4(0.15, 0.25, 0.0, 1.0),
+		vec4(-0.5, -0.5, 0.0, 1.0),
+        vec4(0.15, 0.25, -0.2, 1.0),
         vec4(-0.25, 0.5, 0.0, 1.0),
     };
 	ivec3 triangles[] = {
@@ -35,21 +35,18 @@ int main() {
         r.useShaderProgram(program);
 
         mvp = mat4(1.0f);
-        mvp = translate(mvp, vec3(0.0f, -0.2f, 0.0f));
         r.setUniform(program, "transform", mvp);
         r.setUniform<vec4>(program, "color", vec4(0.79, 0.63, 0.86, 1.0));
 		r.drawObject(box);
 
         mvp = mat4(1.0f);
-        mvp = translate(mvp, vec3(0.1f, 0.1f, 0.0f));
-        mvp = rotate(mvp, radians(100.0f), normalize(vec3(0.0f, 0.0f, 1.0f)));
+        mvp = rotate(mvp, radians(120.0f), normalize(vec3(0.0f, 0.0f, 1.0f)));
         r.setUniform(program, "transform", mvp);
         r.setUniform<vec4>(program, "color", vec4(0.5, 0.5, 0.5, 1.0));
 		r.drawObject(box);
 
         mvp = mat4(1.0f);
-        mvp = translate(mvp, vec3(-0.3f, -0.0f, 0.0f));
-        mvp = rotate(mvp, radians(-140.0f), normalize(vec3(0.0f, 0.0f, 1.0f)));
+        mvp = rotate(mvp, radians(-120.0f), normalize(vec3(0.0f, 0.0f, 1.0f)));
         r.setUniform(program, "transform", mvp);
         r.setUniform<vec4>(program, "color", vec4(0.43, 0.32, 0.24, 1.0));
 		r.drawObject(box);
